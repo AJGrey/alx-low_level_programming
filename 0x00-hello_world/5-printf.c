64cfc671212dec0db8b5a5d7f145960ffc08a6b0
AJGrey <greyajayi@yahoo.com>
2022-09-08 14:19:36 -0700

    Hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-/** 
+/*
  * 
  * main - A program to print the string in the function
  *
@@ -11,5 +11,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
