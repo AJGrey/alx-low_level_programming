08b7fbc05fdb13e1e08f97b41e88f08e5a5af8d6
AJGrey <greyajayi@yahoo.com>
2022-09-08 10:44:59 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,10 @@
  * main - print the string in the put function 
  * This program will print - Programming is like building a multilingual puzzle
  *
- * Return:0
+ * Return: 0
  */
 int main(void)
 {
-	puts("\""Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
