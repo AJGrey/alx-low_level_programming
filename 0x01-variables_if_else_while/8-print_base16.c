a4585676fc3cf98ca863bf5dee2feb433e6a397f
AJGrey <greyajayi@yahoo.com>
2022-09-09 17:44:50 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,7 +12,7 @@ int main(void)
 	chat num;
 	for (num = 0; num <= 9; num++)
 	putchar(num % 10 + '0');
-	for (num = 'a'; ad <= 'f'; ad++)
+	for (ad = 'a'; ad <= 'f'; ad++)
 	       putchar(ad);
 	putchar('\n');
 	return (0);
