1ccd12ebd318563543fa34d35d9cc6d3063be8a4
AJGrey <greyajayi@yahoo.com>
2022-09-09 17:38:54 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 Always
+ *
+ */
+int main(void)
+{
+	int num;
+	chat num;
+	for (num = '0'; num <= 9; num++)
+	putchar(num % 10 + '0');
+	for (num = 'a', ad = 'f', ad++)
+	       putchar(ad);
+	putchar('\n');
+	return (0);
+}
+
