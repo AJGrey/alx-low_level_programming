8e23eda16dbcbec563af8ffb76837e100c15a570
AJGrey <greyajayi@yahoo.com>
2022-09-11 10:28:54 -0700

    The success

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 /**
  * main - entry point
  *
@@ -6,31 +7,29 @@
  */
 int main(void)
 {
-        int hunds, tens, units, k;
+	int hunds, tens, units, k;
 	i ='1';
 	j = '2';
 	for (hunds = '0'; hunds <= '9'; hunds++)
 	{
 		for (tens = '0'; tens <= '9'; tens++)
-        	{
-                	for (units = '0'; units <= '9'; units++)
-                	{
+		{
+			for (units = '0'; units <= '9'; units++)
+			{
 			putchar(hunds);
 			putchar(tens);
-                        putchar(units);
-                        if (huns == '7' && tens == '8' && unit == '9')
+			putchar(units);
+			if (huns == '7' && tens == '8' && unit == '9')
 				break;
 			putchar(',');
-                        putchar(' ');
-                }
+			putchar(' ');
+		}
                 j++;
         }
-        i++;
-       _j++;
-        j = _j;
-				    }
-		putchar('\n');
-putchar('\n');
-return (0);
+	i++;
+	_j++;
+	j = _j;
 }
-	
+	putchar('\n');
+	return (0);
+}
