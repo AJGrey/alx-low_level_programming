f629fe1307ee79a22b8a4d218da5e3bfa19fc3dd
AJGrey <greyajayi@yahoo.com>
2022-09-11 09:23:35 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,15 +7,11 @@
  */
 int main(void)
 {
-	int num, ad;
+	int num;
 
-	for (num ='0'; num <= '9'; num++)
+	for (num ='0'; num <= 'f'; num++)
 	{
-		putchar(num);
-	}
-	for (ad = 'a'; ad <= 'f'; ad++)
-	{
-		putchar(ad);
+		putchar('%x');
 	}
 	putchar('\n');
 	return (0);
