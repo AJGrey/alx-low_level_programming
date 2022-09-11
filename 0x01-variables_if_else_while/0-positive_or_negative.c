1c3c096f6a550141ceaa4a6ea2fe6bc5a715a238
AJGrey <greyajayi@yahoo.com>
2022-09-11 11:00:37 -0700

    Positive thinking

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,12 +14,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	printf('%d' is positive);
-	}
-	else if (n < 0)
 	{
-	printf('%d' is negative);
+		printf('%d is positive', n);
 	}
-	else
-	printf('%d' is zero);
+	else if (n == 0)
+	{
+		printf('%d is zero', n);
+	}
+	else 
+		printf('%d is negative', n);
 	return (0);
+}
