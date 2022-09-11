800c066047eecb03920eb907c4b32b430eea3292
AJGrey <greyajayi@yahoo.com>
2022-09-11 09:35:57 -0700

    P, P and P

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,11 +12,11 @@ int main(void)
 	
 	for (num = '0'; num <= '9'; num++)
 	{
- 		putchar(num);
-		if (num == '9')
-			break;
-		putchar(',');
-		putchar(' ');
+ 	putchar(num);
+	if (num == '9')
+		break;
+	putchar(',');
+	putchar(' ');
 	}
 	putchar('\n');
 	return (0);
