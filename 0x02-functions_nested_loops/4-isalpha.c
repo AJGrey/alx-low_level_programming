e33451d8bae2a534db77ef70e9efa9e79dcab04d
AJGrey <greyajayi@yahoo.com>
2022-09-18 03:35:54 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int _isalpha(int c) - a function to check if c is a letter
+ * int _isalpha - a function to check if c is a letter
  * @c: is the int that will use for the argument of the function
  * Return: 0 Always (Success)
  *
