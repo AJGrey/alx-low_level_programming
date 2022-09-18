cb8fd335c920d9aa86554528a4aa57e926fb798c
AJGrey <greyajayi@yahoo.com>
2022-09-18 03:46:15 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - is a function that will tell the sign a given number 
+ * @n: is the argument that will be used for the function
+ * Return: 0 Alwasy (Success)
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (+1);
+
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
+
