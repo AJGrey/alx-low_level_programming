aac99b36e521ebcfebc933a0226904ac6bba596a
AJGrey <greyajayi@yahoo.com>
2022-09-19 02:19:10 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * jack bauer - a function to print all the hours in the day 
- * Return: 0 Always 
+ * jack_bauer - a function to print all the hours in the day
+ * Return: 0 Always
  *
  */
 void jack_bauer(void)
 {
-	int i,j;
+	int i, j;
+
 	for (i = 0; i < 24; i++)
 	{
 		for (j = 0; j < 60; j++)
@@ -19,13 +20,13 @@ void jack_bauer(void)
 			}
 			else if (i >= 10)
 			{
-				_putchar((i/10) + '0');
-				_putchar((i%10) + '0');
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
 			}
 			if (j < 10)
 			{
 				_putchar(':');
-				_puthcar('0');
+				_putchar('0');
 				_putchar(j + '0');
 			}
 			else if (j > 10)
