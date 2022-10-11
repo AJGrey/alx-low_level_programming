a3feef53d35fafef0227677802f3f1ed043fd916
AJGrey <greyajayi@yahoo.com>
2022-10-10 17:32:07 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define FILE
+
+/**
+ * main - a program that prints the mane of the file
+ *
+ * Return: 0 upon success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
