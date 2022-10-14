38cb4115e8621384a35ad41ff26694750c8b7172
AJGrey <greyajayi@yahoo.com>
2022-10-14 13:32:27 -0700

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - a program to sum all the numbers
+ * @n: numbers
+ *
+ * Return: result upon success
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list mynumbers;
+
+	va_start(mynumbers, n);
+	for (i = 0; i < n; i++)
+		sum = sum + va_arg(mynumbers, int);
+	va_end(mynumbers);
+	return (sum);
+}
+
+
